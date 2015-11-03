a61b7ec80213cc0bcc40724feba6bafcc8c5c13e
Tim <timothy.m.martin@gmail.com>
2015-11-03 11:17:37 -0500

    Initial commit.

---
+++ karma.conf.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var path = require('path');
+var conf = require('./gulp/conf');
+
+var _ = require('lodash');
+var wiredep = require('wiredep');
+
+function listFiles() {
+    var wiredepOptions = _.extend({}, conf.wiredep, {
+        dependencies: true,
+        devDependencies: true
+    });
+
+    return wiredep(wiredepOptions).js
+        .concat([
+            path.join(conf.paths.tmp, '/serve/app/index.module.js'),
+            path.join(conf.paths.src, '/**/*.spec.js'),
+            path.join(conf.paths.src, '/**/*.mock.js'),
+            path.join(conf.paths.src, '/**/*.html')
+        ]);
+}
+
+module.exports = function (config) {
+
+    var configuration = {
+        files: listFiles(),
+        singleRun: true,
+        autoWatch: false,
+        frameworks: ['jasmine'],
+        ngHtml2JsPreprocessor: {
+            stripPrefix: 'src/',
+            moduleName: 'angular1Es6'
+        },
+        browsers: ['PhantomJS'],
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor'
+        ],
+        preprocessors: {
+            'src/**/*.html': ['ng-html2js']
+        }
+    };
+
+    config.set(configuration);
+};
---
+++ src/app/index.route.js
@@ -0,0 +1,15 @@
+function routerConfig ($stateProvider, $urlRouterProvider) {
+    'ngInject';
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'vm'
+        });
+}
+
+export default routerConfig;
